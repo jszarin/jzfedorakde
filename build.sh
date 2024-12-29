86d2ea0d538195d8e13f36010095d42f80a12646
jason <jason@zarin.org>
2024-12-29 15:25:23 -0500

    initial setup

--- build.sh
+++ build.sh
@@ -11,13 +11,24 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+wget https://pkgs.tailscale.com/stable/fedora/tailscale.repo -P /etc/yum.repos.d/
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install \
+           tailscale \
+           mc \
+           fish \
+           lutris \
+           steam \
+           borgbackup \
+           borgmatic \
+           restic 
+           
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
+systemctl enable tailscaled
